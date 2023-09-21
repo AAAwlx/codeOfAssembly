aae39bc8f63ee3f711f8a61d2959369e6e8e59e9
AAAwlx <561639418@qq.com>
2023-09-21 08:44:32 +0800

    o

--- booktool/c08/c08_mbr.asm
+++ booktool/c08/c08_mbr.asm
@@ -15,10 +15,10 @@ SECTION mbr align=16 vstart=0x7c00;标记程序起始地址，在计算物理内
       
          mov ax,[cs:phy_base]            ;计算用于加载用户程序的逻辑段地址 
          mov dx,[cs:phy_base+0x02]
-         mov bx,16        
+         mov bx,16   ;十六字节对齐，除十六即为右移四位     
          div bx            
          mov ds,ax                       ;令DS和ES指向该段以进行操作
-         mov es,ax                        
+         mov es,ax                        ;初始化段寄存器
     
          ;以下读取程序的起始部分 
          xor di,di
@@ -27,10 +27,10 @@ SECTION mbr align=16 vstart=0x7c00;标记程序起始地址，在计算物理内
          call read_hard_disk_0
       
          ;以下判断整个程序有多大
-         mov dx,[2]                      ;曾经把dx写成了ds，花了二十分钟排错 
+         mov dx,[2]                      ;将用户程序的大小放入dx与ax中，程序头部的即为用户程序大小
          mov ax,[0]
          mov bx,512                      ;512字节每扇区
-         div bx
+         div bx                          ;除512,若有余数说明需要回填不是整数，ax中的结果则表示需要的扇区
          cmp dx,0
          jnz @1                          ;未除尽，因此结果比实际扇区数少1 
          dec ax                          ;已经读了一个扇区，扇区总数减1 
@@ -45,7 +45,7 @@ SECTION mbr align=16 vstart=0x7c00;标记程序起始地址，在计算物理内
    @2:
          mov ax,ds
          add ax,0x20                     ;得到下一个以512字节为边界的段地址
-         mov ds,ax  
+         mov ds,ax                       ;防止地址回绕，每读一个扇区逻辑地址加0x20,对应物理地址为0x200
                               
          xor bx,bx                       ;每次读时，偏移地址始终为0x0000 
          inc si                          ;下一个逻辑扇区 
@@ -99,21 +99,17 @@ read_hard_disk_0:                        ;从硬盘读取一个逻辑扇区
          inc dx                          ;0x1f5
          mov ax,di
          out dx,al                       ;LBA地址23~16
-
-         inc dx                          ;0x1f6
-         mov al,0xe0                     ;LBA28模式，主盘
-         or al,ah                        ;LBA地址27~24
-         out dx,al
-
+                                
+         inc dx 
          inc dx                          ;0x1f7
          mov al,0x20                     ;读命令
-         out dx,al
+         out dx,al;将端口0x1f7输出到al中
 
   .waits:
          in al,dx
-         and al,0x88
-         cmp al,0x08
-         jnz .waits                      ;不忙，且硬盘已准备好数据传输 
+         and al,0x88;与操作，除了第7位和第3位全部清零
+         cmp al,0x08;比较是否只有第三位为一
+         jnz .waits                      ;，若只有第三位为一，则表明不忙，且硬盘已准备好数据传输 
 
          mov cx,256                      ;总共要读取的字数
          mov dx,0x1f0
@@ -139,10 +135,9 @@ calc_segment_base:                       ;计算16位段地址
          add ax,[cs:phy_base]
          adc dx,[cs:phy_base+0x02]
          shr ax,4
-         ror dx,4
+         ror dx,4;将dx寄存器中的值由低四位移动到高四位。
          and dx,0xf000
-         or ax,dx
-         
+         or ax,dx;将dx与ax相加并存入ax
          pop dx
          
          ret
